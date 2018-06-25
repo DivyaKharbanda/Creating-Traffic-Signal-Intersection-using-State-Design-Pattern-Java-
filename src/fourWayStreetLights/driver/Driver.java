4caa92bcd03c8a6678fb7e6b881307d16068244c
DivyaKharbanda <divyakharbanda28@gmail.com>
2018-06-24 22:18:20 -0700

    Done with reading the input file line by line and made the State Diagram

---
+++ src/fourWayStreetLights/driver/Driver.java
@@ -0,0 +1,24 @@
+package fourWayStreetLights.driver;
+
+import java.io.File;
+import java.io.IOException;
+
+import fourWayStreetLights.util.FileProcessor;
+
+public class Driver {
+
+	public static void main(String[] args) 
+	{
+		String filename = args[0] ;
+		FileProcessor fp= new FileProcessor();
+		try 
+		{
+			fp.ReadLine(filename);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
